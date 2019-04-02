ab9bdd7e9836d7125be6ba0e2aba9f01c40bebf9
sf585978 <sf585978@gmail.com>
2019-04-02 13:18:00 -0400

    Updated documentation'

---
+++ R/place_geocodes.R
@@ -0,0 +1,12 @@
+#' FIPS Codes and Location Names for Non Counties
+#'
+#' A dataset constructed from 2016 Place and Consolidated City Codes provided by
+#' US Census Bureau.
+#'
+#' @format A dataframe with 19518 rows and 2 columns:
+#'  \describe{
+#'    \item{fips}{FIPS code as character string}
+#'    \item{full_name}{Name in "Location, State" format}
+#'  }
+#' @source \url{https://www.census.gov/geographies/reference-files/2016/demo/popest/2016-fips.html}
+"place_geocodes"
